30036fee00d727d1e742bfcea32b1d0cc7a28f56
h3r0 <50238852+melo936@users.noreply.github.com>
2023-09-29 08:47:40 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# ventoy-config
+# Revision Configuration for Ventoy
+
+This is a test repository for automating some parts of the Windows installation process using Ventoy Auto Install Plugin.
+
+## Features
+
+- OOBE configuration
+  - Disables online account creation
+  - Hides the Network screen
+  - Disables the Privacy screen
+- A custom Script
+  - Disables automatic drivers updating
+  - Disables AllowTelemetry
+- Disables CEIP and Error Reporting
+- Prevents automatic Teams installation
+- Disables dynamic updates and diagnostic data during Windows PE
+
+## Usage
+
+1. Download the latest release of [Ventoy](https://github.com/ventoy/Ventoy/releases)
+2. Configure Secure Boot and partition style
+3. Install Ventoy
+4. Copy the contents of this repository to the root of the Ventoy drive
+5. Move your Windows ISO file(s) to the `WinISO` folder
+6. Boot into Ventoy, go to the `WinISO` folder and select your ISO file
---
+++ ReviSetup/setup.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+reg add "HKCU\Software\Policies\Microsoft\Windows\DriverSearching" /v "DontPromptForWindowsUpdate" /t REG_DWORD /d 1 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\DriverSearching" /v "DontPromptForWindowsUpdate" /t REG_DWORD /d 1 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\DriverSearching" /v "SearchOrderConfig" /t REG_DWORD /d 0 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate" /v "ExcludeWUDriversInQualityUpdate" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Device Metadata" /v "PreventDeviceMetadataFromNetwork" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Device Metadata" /v "PreventDeviceMetadataFromNetwork" /t REG_DWORD /d 1 /f
+
+reg add "HKLM\SYSTEM\ControlSet001\Services\DiagTrack" /v "Start" /t REG_DWORD /d 4 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d "0" /f
+
+exit /b
